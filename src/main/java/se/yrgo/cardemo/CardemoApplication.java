2b3edade163ad975e48dc37839460f8ff016a530
emmi0122 <emiliaidajarleback@gmail.com>
2025-11-15 10:10:50 +0100

    first commit

---
+++ src/main/java/se/yrgo/cardemo/CardemoApplication.java
@@ -0,0 +1,13 @@
+package se.yrgo.cardemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/yrgo/cardemo/CardemoApplicationTests.java
@@ -0,0 +1,13 @@
+package se.yrgo.cardemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
